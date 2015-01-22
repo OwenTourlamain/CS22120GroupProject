5262dcd35b8574274097e34efb6238615a21613c
anj42 <anj42@aber.ac.uk>
2015-01-22 15:14:54 +0000

    this is the UI, all I have so far: working transition buttons, most of the activities etc.

---
+++ PlantsProject/src/com/example/sjdsjdn/SiteCreationScreen.java
@@ -0,0 +1,40 @@
+package com.example.sjdsjdn;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SiteCreationScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.site_creation_screen);
+		
+		/**
+		 * Adding functionality to buttons 
+		 */
+		
+		// "Create" button
+		Button createButton = (Button) findViewById(R.id.createButton);
+		createButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), SpeciesDetailsScreen.class);
+				startActivityForResult(intent, 0);
+			}
+		});
+		
+		// "home" button
+		Button homeButton1 = (Button) findViewById(R.id.homeButton1);
+		homeButton1.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), MainActivity.class);
+				startActivityForResult(intent, 0);
+			}
+		});
+	}
+	
+}
