4ae375e95d1d2be7242de30aeb5063c7826a36cd
anw48 <mail@andrewwynnewilliams.co.uk>
2015-01-23 11:54:18 +0000

    Update AddRecording.java

--- PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/AddRecording.java
+++ PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/AddRecording.java
@@ -1,14 +1,44 @@
 package uk.ac.aber.dcs.CS22120.grouptwelve;
 
+import java.util.ArrayList;
+import android.widget.EditText;
+
 /**
  * Created by adam on 31/12/14.
  */
 public class AddRecording
 {
+    ArrayList<species> plantList = new ArrayList<species>();
+	private EditText edittext;
+	private EditText name;
+	private EditText id;
+	private EditText comment;
+	
+	
     public void addRecording() {}
+    
     public void setGPSLocation() {}
-    public void addSpecies() {}
-    public void addDescription() {}
+    
+    public void addSpecies() {
+        
+        name = (EditText) findViewById(R.id.nameEntryField);
+		id = (EditText) findViewById(R.id.idEntryField);
+		comment = (EditText) findViewById(R.id.commentEntryField);
+
+		species tempSpecies = new species();
+		tempSpecies.setID(id);
+		tempSpecies.setName(name);
+		tempSpecies.setComment(comment);
+		plantList.add(tempSpecies);
+    }
+    
+    public void addDescription() {
+        	edittext = (EditText) findViewById(R.id.commentsEntryField);
+    }
+    
     public void addPhoto() {}
+    
     public void save() {}
+
+    
 }
