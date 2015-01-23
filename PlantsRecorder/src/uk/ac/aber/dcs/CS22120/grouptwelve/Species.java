6ec404b575c052d180322aef16ed6e3c04d21ee9
anw48 <mail@andrewwynnewilliams.co.uk>
2015-01-23 11:55:44 +0000

    Update Species.java

--- PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/Species.java
+++ PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/Species.java
@@ -3,17 +3,38 @@ package uk.ac.aber.dcs.CS22120.grouptwelve;
 /**
  * Created by adam on 01/01/15.
  */
-public class Species
-{
-    private int id;
-    private String name;
+public class Species {
 
-    public Species( int id, String name )
-    {
-        this.id = id;
-        this.name = name;
-    }
+	private int id;
+	private String name;
+	private String comment;
 
-    public int getID() { return id; }
-    public String getName() { return name; }
+	public species() {
+
+	}
+
+	public void setID(int i) {
+		id = i;
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public void setComment(String c) {
+		comment = c;
+
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getComment() {
+		return comment;
+	}
 }
