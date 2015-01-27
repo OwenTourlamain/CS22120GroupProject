4623d3bf0707b5768294b19946fe06a45cef259d
anw48 <mail@andrewwynnewilliams.co.uk>
2015-01-27 12:40:52 +0000

    Update SiteCreationScreen.java

--- PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/androidcode/SiteCreationScreen.java
+++ PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/androidcode/SiteCreationScreen.java
@@ -1,3 +1,10 @@
+/*
+* @(#) SiteCreationScreen.java 1.1 2015-01-27
+*
+* Copyright (c) 2015 Aberystwyth University.
+* All rights reserved.
+*
+*/
 package uk.ac.aber.dcs.CS22120.grouptwelve.androidcode;
 
 import uk.ac.aber.dcs.CS22120.grouptwelve.Record;
@@ -11,6 +18,17 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+
+/**
+* SiteCreationScreen - A class that does something.
+* <p>
+* How it is used
+*
+* @author (name)
+* @since 1.0
+* @version 1.X (put status of version here)
+* @see (ref to related classes)
+*/ 
 public class SiteCreationScreen extends Activity
 {
 	private Record currentRecord;
