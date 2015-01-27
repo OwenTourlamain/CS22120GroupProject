156f6b928e02450dea21c99b5f6df59a9b5fa46b
anw48 <mail@andrewwynnewilliams.co.uk>
2015-01-27 12:11:33 +0000

    Update RecordingList.java

--- PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/RecordingList.java
+++ PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/RecordingList.java
@@ -1,3 +1,11 @@
+/*
+* @(#) SomeClass.java 1.1 2015-01-27
+*
+* Copyright (c) 2015 Aberystwyth University.
+* All rights reserved.
+*
+*/ 
+
 package uk.ac.aber.dcs.CS22120.grouptwelve;
 
 import java.util.ArrayList;
@@ -6,6 +14,16 @@ import java.util.ArrayList;
  * Quite possibly a meaningless class, since species database contains
  * the gatherRecordList method
  */
+ /**
+* RecordingList - A class that ???????
+* <p>
+* How it is used
+*
+* @author (name)
+* @since 1.0
+* @version 1.X (put status of version here)
+* @see (ref to related classes)
+*/
 public class RecordingList
 {
     public ArrayList<Record> gatherRecordList()
