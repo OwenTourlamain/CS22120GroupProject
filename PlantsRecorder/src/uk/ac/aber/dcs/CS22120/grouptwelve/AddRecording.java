4d3bf03772bef606c57ff80d3b7a9bc38f15551c
anw48 <mail@andrewwynnewilliams.co.uk>
2015-01-27 12:01:01 +0000

    Update AddRecording.java

--- PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/AddRecording.java
+++ PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/AddRecording.java
@@ -1,11 +1,26 @@
+/*
+* @(#) AddRecording.java 1.1 2015-01-27
+*
+* Copyright (c) 2015 Aberystwyth University.
+* All rights reserved.
+*
+*/ 
+
 package uk.ac.aber.dcs.CS22120.grouptwelve;
 
 import java.util.ArrayList;
 import android.widget.EditText;
 
-/**
- * Created by adam on 31/12/14.
- */
+ /**
+* SomeClass - A class that does something.
+* <p>
+* How it is used
+*
+* @author Adam 31/12/14
+* @since 1.0
+* @version 1.0 
+* @see (ref to related classes)
+*/
 public class AddRecording
 {
     ArrayList<Species> plantList = new ArrayList<Species>();
