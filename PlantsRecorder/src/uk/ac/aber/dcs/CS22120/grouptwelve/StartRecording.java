34a3996082d64dc2f7210c78831a1743ac1306cf
anw48 <mail@andrewwynnewilliams.co.uk>
2015-01-27 12:22:29 +0000

    Update StartRecording.java

--- PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/StartRecording.java
+++ PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/StartRecording.java
@@ -1,8 +1,29 @@
+
+/*
+* @(#) StartRecording.java 1.1 2015-01-27
+*
+* Copyright (c) 2015 Aberystwyth University.
+* All rights reserved.
+*
+*/ 
 package uk.ac.aber.dcs.CS22120.grouptwelve;
 
 /**
  * Class for creating a recording, may need to be fused with the android class
  */
+ 
+ 
+
+/**
+* StartRecording - A class that creates a recording.
+* <p>
+* How it is used
+*
+* @author (name)
+* @since 1.0
+* @version 1.X (put status of version here)
+* @see (ref to related classes)
+*/ 
 public class StartRecording
 {
     public void startRecording() {}
