67274ccdd30e5629b72fe5aa2068e3ae0b421147
anw48 <mail@andrewwynnewilliams.co.uk>
2015-01-27 12:23:54 +0000

    Update UploadRecords.java

--- PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/UploadRecords.java
+++ PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/UploadRecords.java
@@ -4,7 +4,19 @@ package uk.ac.aber.dcs.CS22120.grouptwelve;
  * this is the class responsible for checking and uploading to the server
  * when an Internet connection is available
  */
-public class UploadRecords
+
+
+
+/**
+* UploadRecords - A class that uploads data to the server.
+* <p>
+* When an internet connection is avaliable the class uploads the data to the server.
+*
+* @author (name)
+* @since 1.0
+* @version 1.X (put status of version here)
+* @see (ref to related classes)
+*/ public class UploadRecords
 {
     public boolean checkForConnection()
     {
