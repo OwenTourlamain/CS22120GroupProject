c6441e21903d2a4fae22bc2728006afa9d4aab24
anw48 <mail@andrewwynnewilliams.co.uk>
2015-01-27 12:26:25 +0000

    Update AddingSpecies.java

--- PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/androidcode/AddingSpecies.java
+++ PlantsRecorder/src/uk/ac/aber/dcs/CS22120/grouptwelve/androidcode/AddingSpecies.java
@@ -1,9 +1,30 @@
+/*
+* @(#) AddingSpecies.java 1.1 2015-01-27
+*
+* Copyright (c) 2015 Aberystwyth University.
+* All rights reserved.
+*
+*/ 
+
 package uk.ac.aber.dcs.CS22120.grouptwelve.androidcode;
 
 /**
  * What needs to be done:
  * 1. methods responsible for operating on data
  */
+ 
+ 
+
+/**
+* AddingSpecies - A class that does something.
+* <p>
+* How it is used
+*
+* @author (name)
+* @since 1.0
+* @version 1.X (put status of version here)
+* @see (ref to related classes)
+*/ 
 
 import java.sql.SQLException;
 
